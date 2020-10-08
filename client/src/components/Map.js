3de09c060012064898f9bb633a97f826ca638f8b
Jesus Tadeo Garcia <tadeoalyta@gmail.com>
2020-10-08 02:03:47 -0500

    search bar is working

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,41 +1,45 @@
-import React, { useCallback, useState, useRef } from 'react';
-import { GoogleMap, useLoadScript, Marker, InfoWindow} from '@react-google-maps/api';
-import {formatRelative} from "date-fns";
-import usePlacesAutocomplete, {getGeoCode, getLatLng} from 'use-places-autocomplete';
-import { Combobox, ComboboxInput, ComboboxPopover, ComboboxList, ComboboxOption} from 'combobox';
-import mapStyle from "./MapStyle"
+import React, { useCallback, useState, useRef } from "react";
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
+// import {formatRelative} from "date-fns";
+import mapStyle from "./MapStyle";
+import Search from "./Search";
 
-const libraries = ["places"]
-const mapContainerStyle={
-  width:"100%",
-  height:"100%"
-}
+const libraries = ["places"];
+const mapContainerStyle = {
+  width: "100%",
+  height: "100%",
+};
 const center = {
   lat: 32.776665,
-  lng: -96.796989
-}
+  lng: -96.796989,
+};
 const options = {
   styles: mapStyle,
   disableDefaultUI: true,
   zoomControl: true,
-}
+};
 
 export default function MapApi() {
-  const {isLoaded, loadError} = useLoadScript({
+  const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
     libraries,
   });
   const [markers, setMarkers] = useState([]);
   const [selected, setSelected] = useState(null);
-  
-  const onMapClick = useCallback(e => {
+
+  const onMapClick = useCallback((e) => {
     setMarkers((current) => [
       ...current,
       {
         lat: e.latLng.lat(),
         lng: e.latLng.lng(),
         time: new Date(),
-      }
+      },
     ]);
   }, []);
 
@@ -44,31 +48,52 @@ export default function MapApi() {
     mapRef.current = map;
   });
 
-  if(loadError) return "Error loading maps";
-  if(!isLoaded) return "Loading Maps";
- 
+  if (loadError) return "Error loading maps";
+  if (!isLoaded) return "Loading Maps";
+
   return (
     <>
-      <div id='map-container'>
-          <GoogleMap mapContainerStyle={mapContainerStyle} 
-          zoom={8} center={center} options={options}
-          onClick={onMapClick} onLoad={onMapLoad}
-          >
-            {markers.map((marker) => (
-              <Marker
-                key={marker.time.toISOString()}
-                position={{lat: marker.lat, lng: marker.lng}}
-                icon={{
-                  color: "blue",
-                  origin: new window.google.maps.Point(0,0),
-                  scaledSize: new window.google.maps.Size(30,30),
-                  anchor: new window.google.maps.Point(15,15)
-                }}
-              />
-              ))}
-          </GoogleMap>
+      <div id="map-container">
+        <Search />
+
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          zoom={8}
+          center={center}
+          options={options}
+          onClick={onMapClick}
+          onLoad={onMapLoad}
+        >
+          {markers.map((marker) => (
+            <Marker
+              key={marker.time.toISOString()}
+              position={{ lat: marker.lat, lng: marker.lng }}
+              icon={{
+                color: "blue",
+                origin: new window.google.maps.Point(0, 0),
+                scaledSize: new window.google.maps.Size(30, 30),
+                anchor: new window.google.maps.Point(15, 15),
+              }}
+              onClick={() => {
+                setSelected(marker);
+              }}
+            />
+          ))}
+
+          {selected ? (
+            <InfoWindow
+              position={{ lat: selected.lat, lng: selected.lng }}
+              onCloseClick={() => {
+                setSelected(null);
+              }}
+            >
+              <div>
+                <h2>MEETING!</h2>
+              </div>
+            </InfoWindow>
+          ) : null}
+        </GoogleMap>
       </div>
     </>
-  )
+  );
 }
-
---
+++ client/src/components/Search.js
@@ -0,0 +1,53 @@
+import React, { useCallback, useState, useRef } from "react";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
+
+export default function Search() {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestion,
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      location: { lat: () => 32.776665, lng: () => -96.796989 },
+      radius: 100 * 1000,
+    },
+  });
+
+  return (
+    <div className="map-container__search">
+      <Combobox
+        onSelect={(address) => {
+          console.log(address);
+        }}
+      >
+        <ComboboxInput
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+          disabled={!ready}
+          placeholder="Enter an address"
+        />
+        <ComboboxPopover>
+          {status === "OK" &&
+            data.map(({ id, description }) => (
+              <ComboboxOption key={id} value={description} />
+            ))}
+        </ComboboxPopover>
+      </Combobox>
+    </div>
+  );
+}
