cc80e5c42ba12d856da1f50550c8d36a30ce8f4b
Jesus Tadeo Garcia <tadeoalyta@gmail.com>
2020-10-08 16:05:11 -0500

    added meetings store

--- backend/app/api/meetings_routes.py
+++ backend/app/api/meetings_routes.py
@@ -4,7 +4,7 @@ from app.models import User, Meeting
 projects_routes = Blueprint('meetings', __name__)
 
 
-@meetings_routes.route('/meetings')
+@meetings_routes.route('/')
 def get_meetings():
     meetings = Meeting.query.all()
     data = [meetings.to_dict() for meeting in meetings]
---
+++ client/src/store/meetings.js
@@ -0,0 +1,40 @@
+const GET_MEETING = "meeting/single";
+const GET_MEETINGS = "meeting/all";
+
+const loadMeeting = (meeting) => {
+  return {
+    type: GET_MEETING,
+    meeting: meeting,
+  };
+};
+
+const loadMeetings = (meetings) => {
+  return {
+    type: GET_MEETINGS,
+    meetings: meetings,
+  };
+};
+
+export const getMeetings = () => {
+  return async (dispatch) => {
+    const res = await fetch("/api/meetings", {
+      method: "GET",
+    });
+    res.data = await res.json();
+    if (res.ok) {
+      dispatch(loadMeetings(res.data.meetings));
+    }
+    return res;
+  };
+};
+
+export default function meetingsReducer(state = {}, action) {
+  switch (action.type) {
+    case GET_MEETING:
+      return { ...state, meeting: action.meeting };
+    case GET_MEETINGS:
+      return { ...state, meetings: action.meetings };
+    default:
+      return state;
+  }
+}
