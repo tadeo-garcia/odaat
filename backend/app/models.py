cb274b0b5f3a8d298a5ff35444513a6d496c1f77
Jesus Tadeo Garcia <tadeoalyta@gmail.com>
2020-10-08 15:29:23 -0500

    added meeting succesfully to database with database.py, updated models

--- backend/app/models.py
+++ backend/app/models.py
@@ -6,108 +6,118 @@ import datetime
 db = SQLAlchemy()
 
 follow_table = db.Table('follow_table',
-  db.Column('follower_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
-  db.Column('followed_id', db.Integer, db.ForeignKey('users.id'), primary_key=True)
-)
+                        db.Column('follower_id', db.Integer, db.ForeignKey(
+                            'users.id'), primary_key=True),
+                        db.Column('followed_id', db.Integer, db.ForeignKey(
+                            'users.id'), primary_key=True)
+                        )
 
 sponsor_table = db.Table('sponsor_table',
-  db.Column('sponsor_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
-  db.Column('sponsee_id', db.Integer, db.ForeignKey('users.id'), primary_key=True)
-)
+                         db.Column('sponsor_id', db.Integer, db.ForeignKey(
+                             'users.id'), primary_key=True),
+                         db.Column('sponsee_id', db.Integer, db.ForeignKey(
+                             'users.id'), primary_key=True)
+                         )
+
 
 class User(db.Model, UserMixin):
-  __tablename__ = 'users'
+    __tablename__ = 'users'
 
-  id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(30), nullable=False)
-  email = db.Column(db.String(55), nullable=False, unique=True)
-  hashed_password = db.Column(db.String(100), nullable=False)
-  zipcode = db.Column(db.Integer)
-  sponsor = db.Column(db.Boolean, default=False)
-  sponsee = db.Column(db.Boolean, default=False)
-  picture = db.Column(db.String(200))
-  bio = db.Column(db.String(1000))
-  created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
-  updated_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(55), nullable=False, unique=True)
+    hashed_password = db.Column(db.String(100), nullable=False)
+    zipcode = db.Column(db.Integer)
+    sponsor = db.Column(db.Boolean, default=False)
+    sponsee = db.Column(db.Boolean, default=False)
+    picture = db.Column(db.String(200))
+    bio = db.Column(db.String(1000))
+    sobriety_date = db.Column(db.Date)
+    created_at = db.Column(db.DateTime, nullable=False,
+                           default=datetime.datetime.utcnow)
+    updated_at = db.Column(db.DateTime, nullable=False,
+                           default=datetime.datetime.utcnow)
 
-  meeting = db.relationship('Meeting', backref='User', lazy=True)
-  followed = db.relationship('User', secondary=follow_table,
-                            primaryjoin=(follow_table.c.follower_id == id),
-                            secondaryjoin=(follow_table.c.followed_id==id),
-                            backref=db.backref('follow_table', lazy='dynamic'), lazy='dynamic')
+    meeting = db.relationship('Meeting', backref='User', lazy=True)
+    followed = db.relationship('User', secondary=follow_table,
+                               primaryjoin=(follow_table.c.follower_id == id),
+                               secondaryjoin=(
+                                   follow_table.c.followed_id == id),
+                               backref=db.backref('follow_table', lazy='dynamic'), lazy='dynamic')
 
-  @property
-  def password(self):
-    return self.hashed_password
+    @property
+    def password(self):
+        return self.hashed_password
 
-  @password.setter
-  def password(self, password):
-    self.hashed_password = generate_password_hash(password)
+    @password.setter
+    def password(self, password):
+        self.hashed_password = generate_password_hash(password)
 
-  def check_password(self, password):
-    return check_password_hash(self.password, password)
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
-  def to_dict(self):
-    return {
-      "id": self.id,
-      "email": self.email,
-      "username":self.username,
-      "zipcode":self.zipcode,
-      "sponsor": self.sponsor,
-      "sponsee": self.sponsee,
-      "picture": self.picture,
-      "bio": self.bio,
-      "created_at": self.created_at.strftime("%B %Y")
-    }
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "username": self.username,
+            "zipcode": self.zipcode,
+            "sponsor": self.sponsor,
+            "sponsee": self.sponsee,
+            "picture": self.picture,
+            "bio": self.bio,
+            "created_at": self.created_at.strftime("%B %Y")
+        }
 
-  def follow(self, user):
-    if not self.is_following(user):
-      self.followed.remove(user)
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.remove(user)
 
-  def unfollow(self, user):
-    if self.is_following(user):
-      self.followed.remove(user)
-  
-  def is_following(self, user):
-    return self.followed.filter(
-      followers.c.followed_id == user.id).count() > 0
-    
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
 
 user_meeting = db.Table('user_meeting',
-  db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
-  db.Column('meeting_id', db.Integer, db.ForeignKey('meetings.id'), primary_key=True))
-  
+                        db.Column('user_id', db.Integer, db.ForeignKey(
+                            'users.id'), primary_key=True),
+                        db.Column('meeting_id', db.Integer, db.ForeignKey('meetings.id'), primary_key=True))
+
+
 class Meeting(db.Model):
-  __tablename__ = 'meetings'
+    __tablename__ = 'meetings'
 
-  id = db.Column(db.Integer, primary_key=True, nullable=False)
-  host_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-  title = db.Column(db.String(1000), nullable=False)
-  description = db.Column(db.String(1000), nullable=False)
-  date = db.Column(db.Date(), nullable=False)
-  time = db.Column(db.Time, nullable=False)
-  location = db.Column(db.String(1000), nullable=False)
-  virtual = db.Column(db.Boolean, default=False)
-  zoom_id = db.Column(db.String, default=None)
-  created_at = db.Column(db.DateTime, nullable=False,
-                         default=datetime.datetime.utcnow)
-  updated_at = db.Column(db.DateTime, nullable=False,
-                         default=datetime.datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    host_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    title = db.Column(db.String(1000), nullable=False)
+    description = db.Column(db.String(1000), nullable=False)
+    date = db.Column(db.Date(), nullable=False)
+    time = db.Column(db.Time, nullable=False)
+    location = db.Column(db.String(1000), nullable=False)
+    virtual = db.Column(db.Boolean, default=False)
+    zoom_id = db.Column(db.String, default=None)
+    official = db.Column(db.Boolean, default=True)
+    created_at = db.Column(db.DateTime, nullable=False,
+                           default=datetime.datetime.utcnow)
+    updated_at = db.Column(db.DateTime, nullable=False,
+                           default=datetime.datetime.utcnow)
 
-  host = db.relationship("User", foreign_keys=[host_id])
-  attendees = db.relationship("User", lazy='subquery')
+    host = db.relationship("User", foreign_keys=[host_id])
+    attendees = db.relationship("User", lazy='subquery')
 
-  def to_dict(self):
-    return {
-        "id": self.id,
-        "title": self.title,
-        "host_id": self.host_id,
-        "description": self.description,
-        "date": self.date,
-        "time": self.time,
-        "location": self.location,
-        "virtual": self.virtual,
-        "zoom_id": self.zoom_id,
-    }
-
-
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "host_id": self.host_id,
+            "description": self.description,
+            "date": self.date,
+            "time": self.time,
+            "location": self.location,
+            "virtual": self.virtual,
+            "zoom_id": self.zoom_id,
+        }
--- backend/database.py
+++ backend/database.py
@@ -1,29 +1,67 @@
+from app.models import User, Meeting
+from app import app, db
 from dotenv import load_dotenv
 load_dotenv()
 
-from app import app, db
-from app.models import User, Meeting
 
 with app.app_context():
-  db.drop_all()
-  db.create_all()
+    db.drop_all()
+    db.create_all()
 
 # USERS
-  ian = User(username = 'Ian', email='ian@odaat.com', password = 'password')
-  javier = User(username = 'Javier', email='javier@odaat.com', password = 'password')
-  dean = User(username = 'Dean', email='dean@odaat.com', password = 'password')
-  angela = User(username = 'Angela', email='angela@odaat.com',  password = 'password')
-  soonmi = User(username='Soon-Mi', email='soon-mi@odaat.com', password='password')
-  alissa = User(username='Alissa', email='alissa@odaat.com', password='password')
-  demo = User(id = 555, username = 'Demo', email='demo@odaat.com', password = 'password')
+    ian = User(username='Ian', email='ian@odaat.com',
+               password='password', zipcode=75214, bio='I love the outdoors and meeting new people', sobriety_date='2019-09-04')
+    javier = User(username='Javier', email='javier@odaat.com',
+                  password='password')
+    dean = User(username='Dean', email='dean@odaat.com', password='password')
+    angela = User(username='Angela', email='angela@odaat.com',
+                  password='password')
+    soonmi = User(username='Soon-Mi', email='soon-mi@odaat.com',
+                  password='password')
+    alissa = User(username='Alissa', email='alissa@odaat.com',
+                  password='password')
+    demo = User(id=555, username='Demo',
+                email='demo@odaat.com', password='password')
 
+    db.session.add(ian)
+    db.session.add(javier)
+    db.session.add(dean)
+    db.session.add(angela)
+    db.session.add(soonmi)
+    db.session.add(alissa)
+    db.session.add(demo)
 
-  db.session.add(ian)
-  db.session.add(javier)
-  db.session.add(dean)
-  db.session.add(angela)
-  db.session.add(soonmi)
-  db.session.add(alissa)
-  db.session.add(demo)
+# MEETINGS
 
-  db.session.commit()
+    meeting1 = Meeting(host_id='1', title='Clean Air Meeting',
+                       description='This meeting is closed; only those who have a desire to stop drinking may attend.',
+                       date='2020-10-31', time='18:00:00', location='16541 Addison Rd, Addison, TX 75001', virtual=True, zoom_id='555xyz', official=True)
+
+    meeting2 = Meeting(host_id='2', title='Serenity Group Meeting',
+                       description='Discussion based meeting. This meeting is open and anyone may attend.',
+                       date='2020-10-31', time='18:00:00', location='106 S Elm St, Sherman, TX 75090', virtual=True, zoom_id='555xyz', official=True)
+
+    meeting3 = Meeting(host_id='3', title='The Gift of Sobriety',
+                       description='This meeting is closed; only those who have a desire to stop drinking may attend.',
+                       date='2020-10-31', time='18:00:00', location='1350 W Walnut Hill Ln Irving, TX 75038', virtual=True, zoom_id='555xyz', official=True)
+
+    meeting4 = Meeting(host_id='4', title='Cornerstone Group Meeting',
+                       description='This meeting is closed; only those who have a desire to stop drinking may attend.',
+                       date='2020-10-31', time='07:00:00', location='9090 Skillman St, Dallas, TX 75243', virtual=True, zoom_id='555xyz', official=True)
+
+    meeting5 = Meeting(host_id='5', title='Clean Air Meeting',
+                       description='This meeting is closed; only those who have a desire to stop drinking may attend.',
+                       date='2020-10-31', time='18:00:00', location='9090 Skillman St, Dallas, TX 75243', virtual=True, zoom_id='555xyz', official=True)
+
+    meeting6 = Meeting(host_id='6', title='Dallas North Group',
+                       description='This meeting is closed; only those who have a desire to stop drinking may attend.',
+                       date='2020-10-31', time='18:00:00', location='101 S Coit Rd  # 210, Richardson, TX 75080', virtual=True, zoom_id='555xyz', official=True)
+
+    db.session.add(meeting1)
+    db.session.add(meeting2)
+    db.session.add(meeting3)
+    db.session.add(meeting4)
+    db.session.add(meeting5)
+    db.session.add(meeting6)
+
+    db.session.commit()
