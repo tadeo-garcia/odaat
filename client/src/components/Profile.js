c9f4768b736a9f2872bfd73a15a327933794ca26
Jesus Tadeo Garcia <tadeoalyta@gmail.com>
2020-10-11 15:48:53 -0500

    beginning layout of profile page done

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { NavLink, Redirect } from "react-router-dom";
 
 export default function Profile() {
+  const currentUser = useSelector((state) => state.auth);
+  console.log(currentUser);
   return (
     <>
-      <div id="profile-container">PROFILE GOES HERE!</div>
+      <div id="profile-container">
+        <div id="profile-container__top">
+          <div id="profile-container__top-banner">Banner goes here.</div>
+        </div>
+        <div id="profile-container__middle">
+          <div id="profile-container__middle-upper">
+            <div id="profile-container__user-details"></div>
+            <div id="profile-container__user-buttons"></div>
+          </div>
+          <div id="profile-container__middle-lower"></div>
+        </div>
+      </div>
     </>
   );
 }
