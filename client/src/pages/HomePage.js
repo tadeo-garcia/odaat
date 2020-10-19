91ca20e2d5015662ce3542a2bc4a7c5dbaa8602f
Jesus Tadeo Garcia <tadeoalyta@gmail.com>
2020-10-18 20:18:16 -0500

    fixed links on splash page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,40 +1,52 @@
-import React from 'react';
-import {useSelector} from 'react-redux'
-import {Link, Redirect} from 'react-router-dom'
-
-
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link, Redirect } from "react-router-dom";
 
 export default function HomePage() {
-const currentUserId = useSelector(state => state.auth.id)
+  const currentUserId = useSelector((state) => state.auth.id);
 
-if (typeof currentUserId === 'number'){
-  return <Redirect to='/dashboard' />
-} 
-  
-return (
-    <> 
-      <div id='homepage__main'>
-        <div id='homepage__top'>
-          <div id='homepage__nav'>
-          
+  if (typeof currentUserId === "number") {
+    return <Redirect to="/dashboard" />;
+  }
+
+  return (
+    <>
+      <div id="homepage__main">
+        <div id="homepage__middle-left"></div>
+        <div id="homepage__middle-right">
+          <div id="homepage__middle-links">
+            <div id="homepage__login">
+              <span> welcome to odaat.</span>
+              <br />
+
+              <br />
+              <span>
+                click{" "}
+                <Link id="homepage__right-link" to="/login">
+                  here
+                </Link>{" "}
+                to sign in
+              </span>
+              <br />
+              <span>
+                click{" "}
+                <Link id="homepage__right-link" to="/signup">
+                  here
+                </Link>{" "}
+                to sign up
+              </span>
+              <br />
+              <span>
+                click{" "}
+                <Link id="homepage__right-link" to="/about">
+                  here
+                </Link>{" "}
+                to learn more
+              </span>
+            </div>
           </div>
-          <div id='homepage__banner'>
-            <span> welcome to odaat.</span>
-            <br/>
-            <span>click <span>here</span> learn more</span>
-            <br />
-            <span>click <Link to='/login'>here</Link> to sign in</span>
-            <br />
-            <span>click <Link to='/signup'>here</Link> to sign up</span>
-          </div>
-        </div>
-        <div id='homepage__middle'>
-
-        </div>
-        <div id='homepage__bottom'>
-
         </div>
       </div>
     </>
-  )
+  );
 }
